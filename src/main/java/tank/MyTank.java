7a3e8becd5f0ccef1f0fdb12eed7cd43ec72e76c
heyu <931680128@qq.com>
2021-02-23 23:13:12 +0800

    坦克大战
    删除敌方坦克后，直接调用repaint()取消闪烁

--- src/main/java/tank/MyTank.java
+++ src/main/java/tank/MyTank.java
@@ -55,6 +55,9 @@ public class MyTank {
         if(!living) {
             //这里不能直接return,要移除tank
             tankFrame.removeTank(this);
+            //加了这个就不会闪烁，不知道为什么。
+            tankFrame.repaint();
+
         }
         switch (dir) {
             case LEFT:
